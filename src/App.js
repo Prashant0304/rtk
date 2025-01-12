c719fdb2053e77bccda2278b3d4001663e07c6f4
unknown <prashantpattanashetti5@gmail.com>
2025-01-12 23:30:48 +0530

    This is todo application used redux tool kit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Counter from "./features/counter/Counter";
+import Posts from "./features/posts/Posts";
+import AddTodo from "./features/todo/AddTodo";
+import TodoList from "./features/todo/TodoList";
 
 
 function App() {
   return (
-    <div>
+    <div className="todoWrapper">
       <Counter/>
+      <hr/>
+    
+      <AddTodo/>
+      <TodoList/>
+      <Posts/>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice';
+import  todoReducer from '../features/todo/todoSlice';
+import postsReducer from '../features/posts/postSlice';
 
 export default configureStore({
   reducer: {
     counter:counterReducer,
+    todo:todoReducer,
+    posts:postsReducer,
   },
 });
